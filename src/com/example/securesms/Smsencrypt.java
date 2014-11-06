f3c1759756d734cc8581ffcc68fcf4672737845e
Erick Boyzo <erickb59@gmail.com>
2014-11-06 16:44:00 -0500

    Add SMS receiver

--- src/com/example/securesms/Smsencrypt.java
+++ src/com/example/securesms/Smsencrypt.java
@@ -3,6 +3,12 @@ package com.example.securesms;
 import android.support.v7.app.ActionBarActivity;
 import android.telephony.SmsManager;
 import android.app.ActionBar;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -11,47 +17,125 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Smsencrypt extends ActionBarActivity {
 
 	private Button sendSMS;
-	private EditText msgTxt;
-	private EditText numTxt;
+	EditText msgTxt;
+	EditText numTxt;
+
+	IntentFilter intentFilter;
+
+	private BroadcastReceiver intentReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+
+			TextView inTxt = (TextView) findViewById(R.id.textMsg);
+			inTxt.setText(intent.getExtras().getString("sms"));
+
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_smsencrypt);
+		/*
+		 * Remove ActionBar code and add permanent xml fix as this will cause
+		 * issues on devices running Froyo 2.2
+		 */
 		ActionBar bar = getActionBar();
 		bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#3fa9f5")));
+
+		intentFilter = new IntentFilter();
+		intentFilter.addAction("SMS_RECEIVED_ACTION");
+
 		sendSMS = (Button) findViewById(R.id.sendbtn);
 		msgTxt = (EditText) findViewById(R.id.message);
 		numTxt = (EditText) findViewById(R.id.number);
+
 		sendSMS.setOnClickListener(new View.OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				sendSMSMessage();
-			}
+				String myMsg = msgTxt.getText().toString();
+				String theNumber = numTxt.getText().toString();
+				sendMsg(theNumber, myMsg);
+				
 
+			}
 		});
+
 	}
 
-	protected void sendSMSMessage() {
+	protected void sendMsg(String theNumber, final String myMsg) {
+		String SENT = "Message Sent";
+		String DELIVERED = "Message Delivered";
 
-		String phoneNo = numTxt.getText().toString();
-		String message = msgTxt.getText().toString();
-		try {
-			SmsManager smsManager = SmsManager.getDefault();
-			smsManager.sendTextMessage(phoneNo, null, message, null, null);
-			Toast.makeText(getApplicationContext(), "SMS sent.",
-					Toast.LENGTH_LONG).show();
-			msgTxt.setText("");
-		} catch (Exception e) {
-			Toast.makeText(getApplicationContext(),
-					"SMS faild, please try again.", Toast.LENGTH_LONG).show();
-			e.printStackTrace();
-		}
+		PendingIntent sentPI = PendingIntent.getBroadcast(this, 0, new Intent(
+				SENT), 0);
+		PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0,
+				new Intent(DELIVERED), 0);
+
+		registerReceiver(new BroadcastReceiver() {
+			public void onReceive(Context arg0, Intent arg1) {
+				switch (getResultCode()) {
+				case Activity.RESULT_OK:
+					Toast.makeText(getBaseContext(), "SMS Sent",
+							Toast.LENGTH_LONG).show();
+					TextView sentText = (TextView) findViewById(R.id.txtSent);
+					sentText.setText(myMsg);
+					numTxt.setText("");
+					msgTxt.setText("");
+					break;
+				case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+					Toast.makeText(getBaseContext(), "Generic Failure",
+							Toast.LENGTH_LONG).show();
+					break;
+				case SmsManager.RESULT_ERROR_NO_SERVICE:
+					Toast.makeText(getBaseContext(), "No Service",
+							Toast.LENGTH_LONG).show();
+					break;
+				}
+			}
+		}, new IntentFilter(SENT));
+
+		registerReceiver(new BroadcastReceiver() {
+			public void onReceive(Context arg0, Intent arg1) {
+
+				switch (getResultCode()) {
+				case Activity.RESULT_OK:
+					Toast.makeText(getBaseContext(), "SMS delivered",
+							Toast.LENGTH_LONG).show();
+
+					break;
+				case Activity.RESULT_CANCELED:
+					Toast.makeText(getBaseContext(), "SMS not Delivered",
+							Toast.LENGTH_LONG).show();
+					break;
+				}
+			}
+		}, new IntentFilter(DELIVERED));
+
+		SmsManager sms = SmsManager.getDefault();
+		sms.sendTextMessage(theNumber, null, myMsg, sentPI, deliveredPI);
+
+	}
+
+	@Override
+	protected void onResume() {
+		// register the receiver
+		registerReceiver(intentReceiver, intentFilter);
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		// unregister the receiver
+		unregisterReceiver(intentReceiver);
+		super.onPause();
 	}
 
 	@Override
